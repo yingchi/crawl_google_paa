fc7a9210c55b7d9ccb1515ff5cac1add477f2e79
Yingchi Pei <yingchi.pei@gmail.com>
2020-05-06 19:03:51 +0800

    update script

--- run_multiple.py
+++ run_multiple.py
@@ -1,29 +1,37 @@
 import os
+import sys
 import pandas as pd
 
 # df = pd.read_csv("input/phrases_interview.csv", header=None, names=["Phrase"])
 df = pd.read_csv("input/phrases.csv", header=0)
-
-TITLES = [
-    "firefighter",
-    "substitute teacher",
-    "dental hygienist",
-    "teacher",
-    "licensed practical nurse",
-    "dental assistant",
-    "nursing assistant",
-    "surgical technician",
-]
+TITLES = [line.rstrip("\n") for line in open("input/titles.csv")]
+# TITLES = [
+#     "firefighter",
+#     "substitute teacher",
+#     "dental hygienist",
+#     "teacher",
+#     "licensed practical nurse",
+#     "dental assistant",
+#     "nursing assistant",
+#     "surgical technician",
+# ]
 
 # TITLES = ["teacher", "project manager", "business analyst", "accountant", "administrative assistant", "product manager"]
 
-for TITLE in TITLES[-1:]:
-    df["query"] = df.apply(lambda x: x["Phrase"].replace("{keyword}", TITLE), axis=1)
-    input_file = "input/faq_queries_{}.csv".format(TITLE.lower().replace(" ", "_"))
-    df[["query"]].to_csv(input_file, index=False, header=False)
-    output_fs_file = "output/fs_" + TITLE.lower().replace(" ", "_") + ".csv"
-    output_paa_file = "output/paa_" + TITLE.lower().replace(" ", "_") + ".csv"
-    command = """poetry run python google_paa.py query {} --output_paa={} --output_fs={} --headless --cleantext --num=10""".format(
-        input_file, output_paa_file, output_fs_file
-    )
-    os.system(command)
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
+        low = int(sys.argv[1])
+        high = int(sys.argv[2])
+    else:
+        low = 0
+        high = len(TITLES)
+    for TITLE in TITLES[low:high]:
+        df["query"] = df.apply(lambda x: x["Phrase"].replace("{keyword}", TITLE), axis=1)
+        input_file = "input/faq_queries_{}.csv".format(TITLE.lower().replace(" ", "_"))
+        df[["query"]].to_csv(input_file, index=False, header=False)
+        output_fs_file = "output/fs_" + TITLE.lower().replace(" ", "_") + ".csv"
+        output_paa_file = "output/paa_" + TITLE.lower().replace(" ", "_") + ".csv"
+        command = """poetry run python google_paa.py query {} --output_paa={} --skip-fs --headless --cleantext --num=10""".format(
+            input_file, output_paa_file
+        )
+        os.system(command)
